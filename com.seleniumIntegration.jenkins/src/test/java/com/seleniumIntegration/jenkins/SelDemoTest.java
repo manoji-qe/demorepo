6db13fc95a498410db9e269d4f850d4745d4785c
DELL <DELL@DELL-PC>
2020-01-02 23:13:07 +0530

    Commit 1

---
+++ com.seleniumIntegration.jenkins/src/test/java/com/seleniumIntegration/jenkins/SelDemoTest.java
@@ -0,0 +1,13 @@
+package com.seleniumIntegration.jenkins;
+
+
+import org.testng.annotations.Test;
+
+public class SelDemoTest {
+@Test
+	public void printSelenium()
+	{
+		System.out.println("This is a selenium demo");
+	}
+
+}
