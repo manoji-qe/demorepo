337f909d5b1c0e00fc098bc80c76ae0a9e3f0957
DELL <DELL@DELL-PC>
2020-01-06 22:38:41 +0530

    commit 2

--- com.seleniumIntegration.jenkins/src/test/java/com/seleniumIntegration/jenkins/SelDemoTest.java
+++ com.seleniumIntegration.jenkins/src/test/java/com/seleniumIntegration/jenkins/SelDemoTest.java
@@ -1,14 +1,39 @@
 package com.seleniumIntegration.jenkins;
 
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SelDemoTest {
-@Test
+	//*[@id="gb"]/div[2]/div[3]/div[1]/div/div[1]/a*//
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void startBrowser() {
+		System.setProperty("webdriver.chrome.driver","E:/Selenium/chromedriver_win32_79/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("https:\\www.gmail.com");
+		//driver.findElement(By.xpath("//*[contains(text(),'Gmail']"));
+	}
+	@Test
 	public void printSelenium()
 	{
 		System.out.println("This is a selenium demo");
 		System.out.println("selenium tools integration");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.close();
 	}
 
 }
